d264e96e915c44f79c5843e373d6371c82341a37
bien aime <bybienaime@gmmail.com>
2020-07-27 17:24:21 +0200

    add reusable textinput and item picker components
    - create styled and reusable compontent

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
-import React from "react"
-import WelcomeScreen from "./app/screens/ListingsScreen"
+import React, {useState} from "react"
+import Screen from "./app/components/Screen"
+import AppPicker from "./app/components/AppPicker"
+import AppTextInput from "./app/components/AppTextInput"
 
+const categories = [
+  {label: "clothes", value: 1},
+  {label: "cars", value: 2},
+  {label: "moto", value: 4},
+]
 export default function App() {
-  return <WelcomeScreen />
+  const [category, setCategory] = useState(categories[1])
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  )
 }
