a4d854cb26ced63cd1ada77061e5693253be7356
bien aime <bybienaime@gmmail.com>
2020-08-15 16:47:37 +0200

    add authentication: signup and login

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import React from "react"
+import React, {useState} from "react"
 import {NavigationContainer} from "@react-navigation/native"
-import AuthNavigator from "./app/navigation/AppNavigator"
+import AuthNavigator from "./app/navigation/AuthNavigator"
 import navigationTheme from "./app/navigation/navigationTheme"
 import OfflineNotice from "./app/components/OfflineNotice"
+import AuthContext from "./app/auth/context"
+import AppNavigator from "./app/navigation/AppNavigator"
+import authStoarge from "./app/auth/storage"
+import {AppLoading} from "expo"
 
 export default function App() {
+  const [user, setUser] = useState()
+  const [isReady, setisReady] = useState(false)
+
+  const restoreUser = async () => {
+    const user = await authStoarge.getUser()
+    if (user) setUser(user)
+  }
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreUser} onFinish={() => setisReady(true)} />
+    )
+
   return (
-    <>
+    <AuthContext.Provider value={{user, setUser}}>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AuthNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   )
 }
--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,16 +1,31 @@
 import React from "react"
 import LottieView from "lottie-react-native"
-import {floor} from "react-native-reanimated"
+import {View, StyleSheet} from "react-native"
+// import {floor} from "react-native-reanimated"
 
 const ActivityIndicator = ({visible = false}) => {
   if (!visible) return null
   return (
-    <LottieView
-      autoPlay
-      loop
-      source={require("../assets/animations/loader.json")}
-    />
+    <View style={styles.overlay}>
+      <LottieView
+        autoPlay
+        loop
+        source={require("../assets/animations/loader.json")}
+      />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  overlay: {
+    backgroundColor: "white",
+    height: "100%",
+    width: "100%",
+    opacity: 0.8,
+    position: "absolute",
+    zIndex: 1,
+    // marginTop: 30,
+  },
+})
 
 export default ActivityIndicator
